9def61d8f3b8b2f88d0d1e5ef8033bb996584328
thelite <prathameshpise6@gmail.com>
2024-06-10 09:54:12 +0530

    #

---
+++ forLoop.js
@@ -0,0 +1,48 @@
+// let myArray = [1,2,3,4];
+// console.log(`${myArray.length} '\n\n`);
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//  console.log(element);
+// }
+
+// it is used to use the the for loop efficiently it automatically iterarte the loop till the values inside the variable is ended for example it is implemented into the string and arrays 
+
+// let myArray = ['prathamesh', 'nikhil', 'ajinkya'];
+// for (const oneArr of myArray) {
+//   console.log(oneArr);
+// }
+
+// loop  on the string  
+
+// let myArray = 'prathamesh'
+// for (const oneArr of myArray) {
+//   console.log(oneArr);
+// }
+
+//const loop = [1,2,2,4,5];
+
+// loop.forEach( function (item){
+//   console.log(item);
+// })
+
+// for each dont return any values like above
+// myArray = [1,2,3,4,5,6];
+
+// const value = myArray.forEach((item) => {
+//   return item
+// });
+// console.log(value);
+
+// to use the return value we use the filter function 
+
+// filter basically used to get the data that we want from the array 
+
+// myArray = [1,2,3,4,5,6,4]
+
+// const newarr = myArray.filter ((num) => {
+//   if (num< 4)
+//     return num
+//   })
+
+//   console.log(newarr);
+
