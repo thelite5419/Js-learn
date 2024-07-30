aa41402a30c52afd29f3495d1cc7935b143510e7
thelite <prathameshpise6@gmail.com>
2024-07-30 20:34:50 +0530

    newLeaning

---
+++ repeatJS/conversion.js
@@ -0,0 +1,16 @@
+let number = "330";
+console.log('number :>> ', number);
+console.log('number :>> ', typeof number);
+
+console.log('-----------');
+
+let fixNum = Number(number);
+console.log('fixNum :>> ', fixNum);
+console.log('fixNum :>> ',typeof fixNum);
+
+console.log('-----------');
+
+let booNum = Boolean(number);
+console.log('booNum :>> ', booNum);
+console.log('booNum :>> ',typeof booNum);
+
